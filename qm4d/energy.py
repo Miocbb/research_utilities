d766a3d462e61d8885c892949ebabf35f19cdaea
YuncaiMei <meiyuncai@hotmai.com>
2020-08-12 15:07:37 -0400

    qm4d/energy.py: add functions to process qm4d output stream
    1. add function to get 1st SCF cycle total energy.
    2. add functions that use qm4d output stream as input.

--- qm4d/energy.py
+++ qm4d/energy.py
@@ -50,3 +50,52 @@ def scf_Etot(qm4d_out):
     except Exception:
         pass
     return float('nan')
+
+
+def f_scf_Etot_step1(f_qm4d_out):
+    """
+    Get 1st cycle SCF total energy from qm4d output stream.
+    @param f_qm4d_out: file stream. The qm4d output file stream.
+    @return float. the SCF total energy. If no results found, return
+        `float('nan')`.
+    """
+    line = f_qm4d_out.readline()
+    while line:
+        if line.startswith('ITER=   1  Energy='):
+            return float(line.split()[3])
+        line = f_qm4d_out.readline()
+    return float('nan')
+
+
+def f_losc_Etot(f_qm4d_out):
+    """
+    Get total energy for post-SCF-LOSC-DFA from qm4d output stream.
+
+    @param f_qm4d_out: file object. qm4d output stream.
+    @return float. the total energy for post-SCF-LOSC. If it failes, return
+        'float('nan')'
+    """
+    line = f_qm4d_out.readline()
+    while line:
+        if 'E_tot_losc' in line:
+            line = line.replace('=', ' ')
+            return float(line.split()[-1])
+        line = f_qm4d_out.readline()
+    return float('nan')
+
+
+def f_scf_Etot(f_qm4d_out):
+    """
+    Get SCF total energy from qm4d output stream.
+
+    @param f_qm4d_out: file object. qm4d output stream.
+    @return float. the total energy for post-SCF-LOSC. If it failes, return
+        'float('nan')'
+    """
+    line = f_qm4d_out.readline()
+    while line:
+        if 'SCF converged successfully. nIter' in line:
+            line = line.replace('=', ' ')
+            return float(line.split()[-1])
+        line = f_qm4d_out.readline()
+    return float('nan')
